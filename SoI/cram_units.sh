d6157abb055935683b40e45c70b1fc4ca8bb8c7b
max <maxdml@cs.duke.edu>
2014-12-03 16:19:57 -0500

    remove useless comments to pimp my commit rate

--- SoI/cram_units.sh
+++ SoI/cram_units.sh
@@ -12,36 +12,3 @@ COMMAND="$SCRIPT -p $i $PARAMS"
 echo $COMMAND
 $COMMAND &
 done
-
-# if [ $( `$SCRIPT -p 0 $PARAMS` ) ] 
-#then
-#    echo "launching ${TYPE} cramming on p0 for ${DUR} sec"
-#fi
-#if [ $( `$SCRIPT -p 1 $PARAMS` & ) ] 
-#then
-#    echo "launching ${TYPE} cramming on p1 for ${DUR} sec"
-#fi
-#if [ $( `$SCRIPT -p 2 $PARAMS` & ) ] 
-#then
-#    echo "launching ${TYPE} cramming on p2 for ${DUR} sec"
-#fi
-#if [ $( `$SCRIPT -p 3 $PARAMS` & ) ] 
-#then
-#    echo "launching ${TYPE} cramming on p3 for ${DUR} sec"
-#fi
-#if [ $( `$SCRIPT -p 4 $PARAMS` & ) ] 
-#then
-#    echo "launching ${TYPE} cramming on p4 for ${DUR} sec"
-#fi
-#if [ $( `$SCRIPT -p 5 $PARAMS` & ) ] 
-#then
-#    echo "launching ${TYPE} cramming on p5 for ${DUR} sec"
-#fi
-#if [ $( `$SCRIPT -p 6 $PARAMS` & ) ] 
-#then
-#    echo "launching ${TYPE} cramming on p6 for ${DUR} sec"
-#fi
-#if [ $( `$SCRIPT -p 7 $PARAMS` & ) ] 
-#then
-#    echo "launching ${TYPE} cramming on p7 for ${DUR} sec"
-#fi
