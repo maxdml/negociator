5f9b82d697eb6910cb8c4465d3df37a568327ad0
bptschaen <bptschaen@gmail.com>
2014-12-05 23:42:58 -0500

    print results and update hterogeneity dataset

--- graphlab/recommender.py
+++ graphlab/recommender.py
@@ -2,8 +2,10 @@
 
 import graphlab as gl
 
-train_data = gl.SFrame.read_csv('/home/max/Documents/learningmaterials/RS_project/graphlab/SimLargeResults.csv');
+train_data = gl.SFrame.read_csv('SimLargeResults.csv');
 
-model = gl.ranking_factorization_recommender.create(train_data, 'user_id', 'item_id', 'performance', num_factors=1)
+model = gl.ranking_factorization_recommender.create(train_data, 'benchmark', 'configuration', 'performance', num_factors=1)
 
-model.recommend(exclude_known = False)
+results = model.recommend(exclude_known = False)
+
+results.print_rows(147,4)
